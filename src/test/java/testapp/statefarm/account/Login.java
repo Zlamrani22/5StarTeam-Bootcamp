02edbebddd6972eb8d5af65a929b5fe6d02e5886
netjava90 <saymak656@gmail.com>
2022-06-24 16:00:36 -0400

    last commit

---
+++ src/test/java/testapp/statefarm/account/Login.java
@@ -0,0 +1,38 @@
+package testapp.statefarm.account;
+
+import base.CommonAPI;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.statefarm.LoginPage;
+
+public class Login extends CommonAPI {
+
+    @Test
+    public void loginWithValidCredentials(){
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.clickOnLogInButton();
+        loginPage.typeUserName("Aksel1506");
+        loginPage.typePassword("TestCase@java10");
+        loginPage.clickLoginButtonSecond();
+        String expectedPageTitle = "Log In";
+        Assert.assertEquals(expectedPageTitle, getPageTitle());
+
+    }
+
+
+    @Test
+    public void loginInvalidCredentials(){
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.clickOnLogInButton();
+        loginPage.typeUserName("Aksel1506");
+        loginPage.typePassword("Test@java");
+        loginPage.clickLoginButtonSecond();
+        String expectedPageTitle = "Log In";
+        Assert.assertEquals(expectedPageTitle, getPageTitle());
+
+    }
+
+}
+
+
+
---
+++ src/test/java/testapp/statefarm/insurance/AutoInsurance.java
@@ -0,0 +1,92 @@
+package testapp.statefarm.insurance;
+
+import base.CommonAPI;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.statefarm.AutoInsurancePage;
+
+public class AutoInsurance extends CommonAPI {
+
+    @Test
+    public void getMotorcycleInsurance(){
+        AutoInsurancePage autoInsurancePage = new AutoInsurancePage(getDriver());
+        autoInsurancePage.clickOnInsurance();
+        autoInsurancePage.clickOnMotorcycle();
+        JavascriptExecutor js = (JavascriptExecutor)getDriver();
+        js.executeScript("window.scrollBy(0,500)", "");
+        autoInsurancePage.type("11010");
+        autoInsurancePage.clickOnFindAgent();
+        String expectedPageTitle = "Find State Farm® Agents Near You - State Farm®";
+        Assert.assertEquals(expectedPageTitle, getPageTitle());
+    }
+    @Test
+    public void getBoatInsurance(){
+        AutoInsurancePage autoInsurancePage = new AutoInsurancePage(getDriver());
+        autoInsurancePage.clickOnInsurance();
+        autoInsurancePage.clickOnBoat();
+        JavascriptExecutor js = (JavascriptExecutor)getDriver();
+        js.executeScript("window.scrollBy(0,500)", "");
+        autoInsurancePage.type("11000");
+        autoInsurancePage.clickOnFindAgent();
+        String expectedPageTitle = "Find State Farm® Agents Near You - State Farm®";
+        Assert.assertEquals(expectedPageTitle, getPageTitle());
+    }
+
+   @Test
+    public void getTrailerInsurance(){
+        AutoInsurancePage autoInsurancePage = new AutoInsurancePage(getDriver());
+        autoInsurancePage.clickOnInsurance();
+        autoInsurancePage.clickOnTrailer();
+        JavascriptExecutor js = (JavascriptExecutor)getDriver();
+        js.executeScript("window.scrollBy(0,500)", "");
+        autoInsurancePage.type("23186");
+        autoInsurancePage.clickOnFindAgent();
+        String expectedPageTitle = "Find State Farm® Agents Near You - State Farm®";
+        Assert.assertEquals(expectedPageTitle, getPageTitle());
+    }
+
+    @Test
+    public void getCarInsurance(){
+        AutoInsurancePage autoInsurancePage = new AutoInsurancePage(getDriver());
+        autoInsurancePage.clickOnInsurance();
+        autoInsurancePage.clickOnCarInsurance();
+        JavascriptExecutor js = (JavascriptExecutor)getDriver();
+        js.executeScript("window.scrollBy(0,500)", "");
+        autoInsurancePage.typeZip("10020");
+        autoInsurancePage.clickOnStartQuote();
+        String expectedPageTitle = "Free Car Insurance Quote - Save on Auto Insurance | State Farm®";
+        Assert.assertEquals(expectedPageTitle, getPageTitle());
+
+    }
+
+    @Test
+    public void getMotorHomeInsurance(){
+        AutoInsurancePage autoInsurancePage = new AutoInsurancePage(getDriver());
+        autoInsurancePage.clickOnInsurance();
+        autoInsurancePage.clickOnMotorHome();
+        waitFor(3);
+        JavascriptExecutor js = (JavascriptExecutor)getDriver();
+        js.executeScript("window.scrollBy(0,500)", "");
+        autoInsurancePage.type("11377");
+        autoInsurancePage.clickOnFindAgent();
+        String expectedPageTitle = "Find State Farm® Agents Near You - State Farm®";
+        Assert.assertEquals(expectedPageTitle, getPageTitle());
+
+    }
+
+    @Test
+    public void getOffRoadVehicle(){
+        AutoInsurancePage autoInsurancePage = new AutoInsurancePage(getDriver());
+        autoInsurancePage.clickOnInsurance();
+        autoInsurancePage.clickOnOffRoad();
+        waitFor(3);
+        JavascriptExecutor js = (JavascriptExecutor)getDriver();
+        js.executeScript("window.scrollBy(0,500)", "");
+        autoInsurancePage.type("25089");
+        autoInsurancePage.clickOnFindAgent();
+        String expectedPageTitle = "Find State Farm® Agents Near You - State Farm®";
+        Assert.assertEquals(expectedPageTitle, getPageTitle());
+    }
+
+}
